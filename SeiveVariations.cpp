13491cd93c298229902bbcfb10881e801f6c7bc2
Paras <paras.nagpal743@gmail.com>
2023-04-25 12:35:22 +0530

    Create SeiveVariations.cpp

---
+++ SeiveVariations.cpp
@@ -0,0 +1,37 @@
+const int N = 1e6 + 7 ;
+vector<int> isPrime(N,true);
+
+void seive(){
+     isPrime[0] = isPrime[1] = false ;
+     for(int i = 2 ; i < N ; ++i){
+         if(isPrime[i]){
+             for(int j = i*i ; j < N ; j+=i){
+                isPrime[j] = false ;
+             }
+         }
+     }
+}
+
+vector<int> lowestPrime(N,N) ;
+void seiveLPF(){
+    isPrime[0] = isPrime[1] = false ;
+     for(int i = 2 ; i < N ; ++i){
+         if(isPrime[i]){
+             for(int j = i*i ; j < N ; j+=i){
+                isPrime[j] = false ;
+                if(lowestPrime[j] == j){
+                    lowestPrime[j] = i ;
+                }
+             }
+         }
+     }
+}
+
+vector<int> PrimeFactors(int num){
+    vector<int> primes ;
+    while(num > 1){
+        primes.pb(lowestPrime[num]);
+        num = num/lowestPrime[num];
+    }
+    return primes ;
+}
