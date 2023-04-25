ed75e15ad0ee2067f43dd369ad32f2391d7fadf4
Paras <paras.nagpal743@gmail.com>
2023-04-25 12:40:35 +0530

    Create BinaryExponentiation.cpp

---
+++ BinaryExponentiation.cpp
@@ -0,0 +1,9 @@
+int binpow(int a,int b){
+     if (b == 0)
+        return 1;
+    int res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res ;
+} 
