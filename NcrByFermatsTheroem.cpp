5ebde435428360efb0973cff47b6a558616225dd
Paras <paras.nagpal743@gmail.com>
2023-04-25 12:37:57 +0530

    Create NcrByFermatsTheroem.cpp

---
+++ NcrByFermatsTheroem.cpp
@@ -0,0 +1,31 @@
+ // Returns n^(-1) mod p
+unsigned long long modInverse(unsigned long long n,  
+                                            int p)
+{
+    return power(n, p - 2, p);
+}
+  
+// Returns nCr % p using Fermat's little
+// theorem.
+unsigned long long nCrModPFermat(unsigned long long n,
+                                 int r, int p)
+{
+    // If n<r, then nCr should return 0
+    if (n < r)
+        return 0;
+    // Base case
+    if (r == 0)
+        return 1;
+  
+    // Fill factorial array so that we
+    // can find all factorial of r, n
+    // and n-r
+    unsigned long long fac[n + 1];
+    fac[0] = 1;
+    for (int i = 1; i <= n; i++)
+        fac[i] = (fac[i - 1] * i) % p;
+  
+    return (fac[n] * modInverse(fac[r], p) % p
+            * modInverse(fac[n - r], p) % p)
+           % p;
+}
