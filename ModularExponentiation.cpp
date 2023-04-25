5666510690f5d8a6f5dbd9219519f153e3b9a6b1
Paras <paras.nagpal743@gmail.com>
2023-04-25 12:39:11 +0530

    Create ModularExponentiation.cpp

---
+++ ModularExponentiation.cpp
@@ -0,0 +1,21 @@
+unsigned long long power(unsigned long long x, 
+                                  int y, int p)
+{
+    unsigned long long res = 1; // Initialize result
+  
+    x = x % p; // Update x if it is more than or
+    // equal to p
+  
+    while (y > 0) 
+    {
+      
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res * x) % p;
+  
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
